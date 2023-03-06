77c5591ceec8b0d1609eeb4aacdf6ce1136c9df8
Bernardo Lira <harritolira@gmail.com>
2023-03-05 19:31:49 -0600

    Debug off

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a@6xv7g9##eo@vyqsksjo6_)yd6^6+&zjb_c=0bf+!%llih&%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
